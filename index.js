7e775c4522bfed969705f92480d4e2640c8b2e87
omarodc <2490065+omarodc@users.noreply.github.com>
2024-02-18 21:44:26 -0600

    add reset functionality and limit of the grid

--- index.js
+++ index.js
@@ -18,13 +18,38 @@ function createCanvas() {
   const newColor = document.querySelectorAll(".size");
   for (let color of newColor) {
     color.addEventListener("mouseover", () => {
-      color.style.backgroundColor = "green";
+      color.style.backgroundColor = "black";
     });
   }
 }
 createCanvas();
 
+// reset grid when button is clicked and input size.
+function reset() {
+  document
+    .querySelectorAll(".size")
+    .forEach((e) => e.parentNode.removeChild(e));
+}
+// ask user for a grid size.
 btn.addEventListener("click", () => {
-  const input = prompt("Enter a new size for the grid max(100x100):");
-  console.log(input);
+  const input = parseInt(
+    prompt("Enter a new size for the grid min(16x16),max(100x100):")
+  );
+  column = input;
+  row = input;
+  // check if input is in range of accepted value.
+  if (column >= 16 && row >= 16 && column <= 100 && row <= 100) {
+    reset();
+    createCanvas();
+  }
 });
+
+// TODO:
+// Extra credit
+// Transform the behavior of a square when interacting with the mouse by introducing a series of modifications.
+
+// Rather than a color change from black to white, each interaction should randomize the square’s RGB value entirely.
+// Additionally, implement a progressive darkening effect where each interaction
+// adds 10% more black or color to the square. The objective is to achieve a completely
+//  black square only after ten interactions.
+// You can choose to do either one or both of these challenges, it’s up to you.
